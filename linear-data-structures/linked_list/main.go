42df14e6618d36b58742c45ab3dfb614bb8979b6
Tolulope <lanre.toluwa@gmail.com>
2021-04-01 17:11:04 +0100

    changed PrintList to function and wrote insertBefore method

--- linear-data-structures/linked_list/main.go
+++ linear-data-structures/linked_list/main.go
@@ -30,9 +30,10 @@ func (l *LinkedList) getLastNode() *Node {
 }
 
 
-func (l *LinkedList) addToEnd(value int) {
+func (l *LinkedList) append(value int) {
 	lastNode := l.getLastNode()
 	newNode := &Node{data: value}
+
 	if lastNode != nil {
 		lastNode.next = newNode
 	}
@@ -40,12 +41,64 @@ func (l *LinkedList) addToEnd(value int) {
 }
 
 
-func (l LinkedList) PrintList() {
+func PrintList(l LinkedList) {
 	for picker := l.head; picker != nil; picker = picker.next {
 		fmt.Printf("%d -> ", picker.data)
 	}
-
 	fmt.Println("end")
+}
+
+
+func (l LinkedList) getNodeWithValue(value int) *Node {
+	var nodeToFind *Node
+
+	for picker := l.head; picker != nil; picker = picker.next {
+		if picker.data == value {
+			nodeToFind = picker
+			break
+		}
+	}	
+
+	return nodeToFind
+}
+
+
+func (l LinkedList) insertAfter(value, insertValue int) {
+	var nodeToInsert *Node
+	for picker := l.head; picker != nil; picker = picker.next {
+
+		if picker.data == value {
+
+			nodeToInsert = &Node{data: insertValue}
+			nextNode := picker.next
+			picker.next = nodeToInsert
+			nodeToInsert.next = nextNode
+			break
+
+		}
+	}	
+
+	if nodeToInsert == nil {
+		fmt.Printf("value %v is not in LinkedList\n", value)
+	}
+}
+
+
+func (l *LinkedList) insertBefore(value, insertValue int) {
+	if l.head.data == value {
+		l.prepend(insertValue)
+		return
+	}
+
+	for picker := l.head; picker.next != nil; picker = picker.next {
+		if picker.next.data == value {
+			nodeToInsert := &Node{data: insertValue}
+			nextNode := picker.next
+			picker.next = nodeToInsert
+			nodeToInsert.next = nextNode
+			break
+		}
+	}
 }
 
 
@@ -54,20 +107,27 @@ func main() {
 
 	value1 := 7
 	newList.prepend(value1)
-	newList.PrintList() // 7
+	PrintList(newList) // 7
 	fmt.Printf("The last value is %v \n", newList.getLastNode())
 
 	value2 := 5
 	newList.prepend(value2)
-	newList.PrintList()  // 5 -> 7
+	PrintList(newList)  // 5 -> 7
 
 	value3 := 3
 	newList.prepend(value3)
-	newList.PrintList() // 3 -> 5 -> 7
+	PrintList(newList) // 3 -> 5 -> 7
 
 	value4 := 2
-	newList.addToEnd(value4)
-	newList.PrintList() // 3 -> 5 -> 7 -> 2
+	newList.append(value4)
+	PrintList(newList) // 3 -> 5 -> 7 -> 2
+
+	newList.insertAfter(3, 1)
+	PrintList(newList)
+	
+
+	newList.insertBefore(5, 9)
+	PrintList(newList) // 3 -> 5 -> 7 -> 9 -> 2
 
 	fmt.Printf("The last node is %v \n", newList.getLastNode())
 }
